43529849c20a1c02836abef740861508a132d2b7
Ian Ynda-Hummel <ianynda@gmail.com>
2012-07-14 13:44:03 -0700

    fill out adding people behavior and table change animations

--- Stab/SIPersonViewController.m
+++ Stab/SIPersonViewController.m
@@ -10,14 +10,22 @@
 
 #import "SIPerson.h"
 
-@interface SIPersonViewController () <UITableViewDataSource, UITableViewDelegate>
+typedef enum {
+    WSPersonViewControllerSectionAdd,
+    WSPersonViewControllerSectionPeople,
+    WSPersonViewControllerSectionCount
+} WSPersonViewControllerSection;
+
+@interface SIPersonViewController () <UITableViewDataSource, UITableViewDelegate, UITextFieldDelegate>
 @property (nonatomic, retain) IBOutlet UITableView *tableView;
+@property (nonatomic, retain) IBOutlet UITableViewCell *addPersonCell;
 
 @property (nonatomic, retain) NSMutableArray *people;
 @end
 
 @implementation SIPersonViewController
 @synthesize tableView = _tableView;
+@synthesize addPersonCell = _addPersonCell;
 @synthesize people = _people;
 
 - (void)awakeFromNib {
@@ -31,6 +39,7 @@
     self.tableView.dataSource = nil;
     self.tableView.delegate = nil;
     self.tableView = nil;
+    self.addPersonCell = nil;
 }
 
 #pragma mark - Editing
@@ -38,15 +47,31 @@
 - (void)setEditing:(BOOL)editing animated:(BOOL)animated {
     [super setEditing:editing animated:animated];
     [self.tableView setEditing:editing animated:animated];
+    [self.tableView reloadSections:[NSIndexSet indexSetWithIndex:WSPersonViewControllerSectionAdd]
+                  withRowAnimation:UITableViewRowAnimationBottom];
 }
 
 #pragma mark - UITableViewDataSource
+                                    
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return WSPersonViewControllerSectionCount;
+}
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return [self.people count];
+    switch (section) {
+        case WSPersonViewControllerSectionAdd:
+            return (self.tableView.editing ? 1 : 0);
+        case WSPersonViewControllerSectionPeople:
+            return [self.people count];
+    }
+
+    return 0;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.section == WSPersonViewControllerSectionAdd)
+        return self.addPersonCell;
+
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"SIPersonTableViewCell"];
     SIPerson *person = [self.people objectAtIndex:indexPath.row];
 
@@ -56,5 +81,42 @@
 }
 
 #pragma mark - UITableViewDelegate
+
+- (BOOL)tableView:(UITableView *)tableView shouldIndentWhileEditingRowAtIndexPath:(NSIndexPath *)indexPath {
+    return NO;
+}
+
+- (UITableViewCellEditingStyle)tableView:(UITableView *)tableView editingStyleForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if (indexPath.section == WSPersonViewControllerSectionAdd)
+        return UITableViewCellEditingStyleNone;
+
+    return UITableViewCellEditingStyleDelete;
+}
+
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+    [self.people removeObjectAtIndex:indexPath.row];
+    [self.tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath]
+                          withRowAnimation:UITableViewRowAnimationFade];
+}
+
+#pragma mark - UITextFieldDelegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    // Create and insert a person with the appropriate name
+    SIPerson *person = [[SIPerson alloc] init];
+    person.name = textField.text;
+    [self.people insertObject:person atIndex:0];
+
+    // Insert a row in the table for the new person
+    NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0
+                                                inSection:WSPersonViewControllerSectionPeople];
+    [self.tableView insertRowsAtIndexPaths:[NSArray arrayWithObject:indexPath]
+                          withRowAnimation:UITableViewRowAnimationTop];
+
+    // Clear the text in the text field
+    textField.text = @"";
+
+    return NO;
+}
 
 @end
