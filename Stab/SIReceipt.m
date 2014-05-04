57fa1fef64d6de3f3ec9cec88003c6c636a2700a
Ian Ynda-Hummel <ianynda@gmail.com>
2014-05-04 11:58:18 -0400

    Remove some unnecessary commented out code

--- Stab/SIReceipt.m
+++ Stab/SIReceipt.m
@@ -127,48 +127,6 @@ static SIReceipt *sharedReceipt;
     });
 }
 
-//UIImage *gs_convert_image (UIImage *src_img) {
-//    CGColorSpaceRef d_colorSpace = CGColorSpaceCreateDeviceRGB();
-//    /*
-//     * Note we specify 4 bytes per pixel here even though we ignore the
-//     * alpha value; you can't specify 3 bytes per-pixel.
-//     */
-//    size_t d_bytesPerRow = src_img.size.width * 4;
-//    unsigned char * imgData = (unsigned char*)malloc(src_img.size.height*d_bytesPerRow);
-//    CGContextRef context =  CGBitmapContextCreate(imgData,
-//                                                  src_img.size.width,
-//                                                  src_img.size.height,
-//                                                  8,
-//                                                  d_bytesPerRow,
-//                                                  d_colorSpace,
-//                                                  kCGImageAlphaNoneSkipFirst);
-//
-//    UIGraphicsPushContext(context);
-//    // These next two lines 'flip' the drawing so it doesn't appear upside-down.
-//    CGContextTranslateCTM(context, 0.0, src_img.size.height);
-//    CGContextScaleCTM(context, 1.0, -1.0);
-//    // Use UIImage's drawInRect: instead of the CGContextDrawImage function, otherwise you'll have issues when the source image is in portrait orientation.
-//    [src_img drawInRect:CGRectMake(0.0, 0.0, src_img.size.width, src_img.size.height)];
-//    UIGraphicsPopContext();
-//
-//    /*
-//     * At this point, we have the raw ARGB pixel data in the imgData buffer, so
-//     * we can perform whatever image processing here.
-//     */
-//
-//
-//    // After we've processed the raw data, turn it back into a UIImage instance.
-//    CGImageRef new_img = CGBitmapContextCreateImage(context);
-//    UIImage *convertedImage = [[UIImage alloc] initWithCGImage:new_img];
-//
-//    CGImageRelease(new_img);
-//    CGContextRelease(context);
-//    CGColorSpaceRelease(d_colorSpace);
-//    free(imgData);
-//
-//    return convertedImage;
-//}
-
 - (void)addEntryWithName:(NSString *)name cost:(NSNumber *)cost {
     SIReceiptItem *item = [[SIReceiptItem alloc] init];
 
